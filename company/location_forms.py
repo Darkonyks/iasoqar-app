3289a44b45b617f2956057f56f573d81275de98c
Darko Nedic <nedic.darko@geodigit.rs>
2026-03-06 10:58:10 +0100

    feat: refactor LocationForm and views for improved company handling and add development docker-compose file

--- company/location_forms.py
+++ company/location_forms.py
@@ -1,76 +1,96 @@
 from django import forms
-from django.utils.translation import gettext_lazy as _
 from .company_models import OstalaLokacija, Company
 
+# Placeholder-i za polja forme
+FIELD_PLACEHOLDERS = {
+    'name': 'Unesite naziv lokacije',
+    'street': 'Unesite ulicu',
+    'street_number': 'Unesite broj',
+    'city': 'Unesite grad',
+    'postal_code': 'Unesite poštanski broj',
+    'country': 'Unesite državu',
+    'notes': 'Dodatne napomene (opciono)',
+}
+
+FIELD_LABELS = {
+    'name': 'Naziv lokacije',
+    'street': 'Ulica',
+    'street_number': 'Broj',
+    'city': 'Grad',
+    'postal_code': 'Poštanski broj',
+    'country': 'Država',
+    'notes': 'Napomena',
+}
+
+DEFAULT_COUNTRY = 'Srbija'
+
+
 class LocationForm(forms.ModelForm):
-    """Forma za kreiranje i ažuriranje ostalih lokacija kompanije"""
-    
+    """Forma za kreiranje i ažuriranje ostalih lokacija kompanije."""
+
     class Meta:
         model = OstalaLokacija
         fields = [
-            'company', 'name', 'street', 'street_number', 'city', 'postal_code', 'country', 'notes'
+            'company', 'name', 'street', 'street_number',
+            'city', 'postal_code', 'country', 'notes',
         ]
-        widgets = {
-            'name': forms.TextInput(attrs={
-                'class': 'form-control',
-                'placeholder': 'Unesite naziv lokacije'
-            }),
-            'street': forms.TextInput(attrs={
-                'class': 'form-control',
-                'placeholder': 'Unesite ulicu'
-            }),
-            'street_number': forms.TextInput(attrs={
-                'class': 'form-control',
-                'placeholder': 'Unesite broj'
-            }),
-            'city': forms.TextInput(attrs={
-                'class': 'form-control',
-                'placeholder': 'Unesite grad'
-            }),
-            'postal_code': forms.TextInput(attrs={
-                'class': 'form-control',
-                'placeholder': 'Unesite poštanski broj'
-            }),
-            'country': forms.TextInput(attrs={
-                'class': 'form-control',
-                'placeholder': 'Unesite državu'
-            }),
-            'notes': forms.Textarea(attrs={
-                'class': 'form-control',
-                'rows': 3,
-                'placeholder': 'Dodatne napomene (opciono)'
-            })
-        }
-        labels = {
-            'name': 'Naziv lokacije',
-            'street': 'Ulica',
-            'street_number': 'Broj',
-            'city': 'Grad',
-            'postal_code': 'Poštanski broj',
-            'country': 'Država',
-            'notes': 'Napomena'
-        }
-    
+        labels = FIELD_LABELS
+
     def __init__(self, *args, **kwargs):
         company_id = kwargs.pop('company_id', None)
         super().__init__(*args, **kwargs)
         self._company_from_param = None
-        
-        # Postavi podrazumevanu vrednost za državu
+
+        self._apply_bootstrap_classes()
+        self._set_default_country()
+        self._bind_company(company_id)
+
+    # ------------------------------------------------------------------
+    # Privatne pomoćne metode
+    # ------------------------------------------------------------------
+
+    def _apply_bootstrap_classes(self):
+        """Doda 'form-control' klasu i placeholder na svako vidljivo polje."""
+        for field_name, field in self.fields.items():
+            if isinstance(field.widget, forms.HiddenInput):
+                continue
+            field.widget.attrs.setdefault('class', 'form-control')
+            if field_name in FIELD_PLACEHOLDERS:
+                field.widget.attrs.setdefault('placeholder', FIELD_PLACEHOLDERS[field_name])
+            # Textarea za napomene
+            if field_name == 'notes' and not isinstance(field.widget, forms.Textarea):
+                field.widget = forms.Textarea(attrs={
+                    'class': 'form-control',
+                    'rows': 3,
+                    'placeholder': FIELD_PLACEHOLDERS.get('notes', ''),
+                })
+            elif field_name == 'notes':
+                field.widget.attrs.setdefault('rows', 3)
+
+    def _set_default_country(self):
+        """Postavi podrazumevanu državu za nove instance."""
         if not self.instance.pk and not self.initial.get('country'):
-            self.initial['country'] = 'Srbija'
-        
-        if company_id:
-            try:
-                company = Company.objects.get(pk=company_id)
-                self._company_from_param = company
-                self.fields['company'].widget = forms.HiddenInput()
-                self.fields['company'].required = False
-                self.fields['company'].initial = company
-                if not self.instance.pk:
-                    self.instance.company = company
-            except Company.DoesNotExist:
-                pass
+            self.initial['country'] = DEFAULT_COUNTRY
+
+    def _bind_company(self, company_id):
+        """Poveži formu sa konkretnom kompanijom ako je prosleđen ID."""
+        if not company_id:
+            return
+        try:
+            company = Company.objects.get(pk=company_id)
+        except Company.DoesNotExist:
+            return
+
+        self._company_from_param = company
+        self.fields['company'].widget = forms.HiddenInput()
+        self.fields['company'].required = False
+        self.fields['company'].initial = company
+        if not self.instance.pk:
+            self.instance.company = company
+
+    # ------------------------------------------------------------------
+    # Validacija
+    # ------------------------------------------------------------------
 
     def clean_company(self):
         company = self.cleaned_data.get('company')
--- company/location_views.py
+++ company/location_views.py
@@ -1,138 +1,148 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from django.contrib import messages
-from django.shortcuts import get_object_or_404, redirect
-from django.utils.translation import gettext_lazy as _
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .company_models import OstalaLokacija, Company
 from .location_forms import LocationForm
 
-class LocationListView(ListView):
+if TYPE_CHECKING:
+    from django.http import HttpRequest
+
+
+# ======================================================================
+# Mixin – zajednicka logika za rad sa kompanijom iz query-stringa
+# ======================================================================
+
+class CompanyFromQueryMixin:
+    """Izvlači kompaniju iz GET parametra `company` i dodaje je u kontekst."""
+
+    request: HttpRequest  # type hint za Pylance
+
+    def _get_company_or_none(self) -> Company | None:
+        company_id = self.request.GET.get('company')
+        if not company_id:
+            return None
+        return Company.objects.filter(pk=company_id).first()
+
+
+# ======================================================================
+# Views
+# ======================================================================
+
+class LocationListView(LoginRequiredMixin, CompanyFromQueryMixin, ListView):
     model = OstalaLokacija
     template_name = 'company/location/location-list.html'
     context_object_name = 'locations'
-    
+
     def get_queryset(self):
-        queryset = super().get_queryset()
-        
-        # Filtriranje po kompaniji ako je prosleđen ID kompanije
-        company_id = self.request.GET.get('company', None)
-        if company_id:
-            queryset = queryset.filter(company_id=company_id)
-            
-        return queryset.select_related('company')
-    
+        qs = super().get_queryset().select_related('company')
+        company = self._get_company_or_none()
+        if company:
+            qs = qs.filter(company=company)
+        return qs
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        
-        # Dodajemo informacije o kompaniji ako je filtriranje po kompaniji
-        company_id = self.request.GET.get('company', None)
-        if company_id:
-            try:
-                company = Company.objects.get(pk=company_id)
-                context['company'] = company
-                context['title'] = f'Lokacije kompanije {company.name}'
-            except Company.DoesNotExist:
-                pass
+        company = self._get_company_or_none()
+        if company:
+            context['company'] = company
+            context['title'] = f'Lokacije kompanije {company.name}'
         else:
             context['title'] = 'Sve lokacije'
-            
         return context
 
-class LocationDetailView(DetailView):
+
+class LocationDetailView(LoginRequiredMixin, DetailView):
     model = OstalaLokacija
+    object: OstalaLokacija
     template_name = 'company/location/location-detail.html'
     context_object_name = 'location'
-    
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['title'] = self.object.name
         return context
 
-class LocationCreateView(CreateView):
+
+class LocationCreateView(LoginRequiredMixin, CompanyFromQueryMixin, CreateView):
     model = OstalaLokacija
+    object: OstalaLokacija
     form_class = LocationForm
     template_name = 'company/location/location-form.html'
-    
+
     def get_form_kwargs(self):
         kwargs = super().get_form_kwargs()
-        
-        # Prosleđujemo company_id formi ako je prosleđen u URL-u
-        company_id = self.request.GET.get('company', None)
-        if company_id:
-            kwargs['company_id'] = company_id
-            
+        company = self._get_company_or_none()
+        if company:
+            kwargs['company_id'] = company.pk
         return kwargs
-    
+
     def get_initial(self):
         initial = super().get_initial()
-        
-        # Ako je prosleđen ID kompanije, postavljamo je kao inicijalnu vrednost
-        company_id = self.request.GET.get('company', None)
-        if company_id:
-            initial['company'] = company_id
-            
+        company = self._get_company_or_none()
+        if company:
+            initial['company'] = company.pk
         return initial
-    
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['title'] = 'Nova lokacija'
         context['submit_text'] = 'Sačuvaj'
-        
-        # Ako je prosleđen ID kompanije, dodajemo informacije o kompaniji
-        company_id = self.request.GET.get('company', None)
-        if company_id:
-            try:
-                company = Company.objects.get(pk=company_id)
-                context['company'] = company
-                context['title'] = f'Nova lokacija za kompaniju {company.name}'
-            except Company.DoesNotExist:
-                pass
-                
+        company = self._get_company_or_none()
+        if company:
+            context['company'] = company
+            context['title'] = f'Nova lokacija za kompaniju {company.name}'
         return context
-    
+
     def get_success_url(self):
-        # Ako je lokacija kreirana za određenu kompaniju, vraćamo se na detalje te kompanije
         if self.object.company:
-            messages.success(self.request, f'Lokacija "{self.object.name}" je uspešno kreirana.')
             return reverse_lazy('company:detail', kwargs={'pk': self.object.company.pk})
         return reverse_lazy('company:location_list')
-    
-    def form_valid(self, form):
-        messages.success(self.request, f'Lokacija "{form.instance.name}" je uspešno kreirana.')
-        return super().form_valid(form)
 
-class LocationUpdateView(UpdateView):
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        messages.success(self.request, f'Lokacija "{self.object.name}" je uspešno kreirana.')
+        return response
+
+
+class LocationUpdateView(LoginRequiredMixin, UpdateView):
     model = OstalaLokacija
+    object: OstalaLokacija
     form_class = LocationForm
     template_name = 'company/location/location-form.html'
-    
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['title'] = f'Izmena lokacije {self.object.name}'
         context['submit_text'] = 'Sačuvaj izmene'
         context['company'] = self.object.company
         return context
-    
-    def get_success_url(self):
-        messages.success(self.request, f'Lokacija "{self.object.name}" je uspešno izmenjena.')
-        return reverse_lazy('company:detail', kwargs={'pk': self.object.company.pk})
-    
-    def form_valid(self, form):
-        messages.success(self.request, f'Lokacija "{form.instance.name}" je uspešno izmenjena.')
-        return super().form_valid(form)
 
-class LocationDeleteView(DeleteView):
+    def get_success_url(self):
+        return reverse_lazy('company:detail', kwargs={'pk': self.object.company.pk})
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        messages.success(self.request, f'Lokacija "{self.object.name}" je uspešno izmenjena.')
+        return response
+
+
+class LocationDeleteView(LoginRequiredMixin, DeleteView):
     model = OstalaLokacija
+    object: OstalaLokacija
     template_name = 'company/location/location-confirm-delete.html'
     context_object_name = 'location'
-    
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['title'] = f'Brisanje lokacije {self.object.name}'
         return context
-    
+
     def get_success_url(self):
         company = self.object.company
         messages.success(self.request, f'Lokacija "{self.object.name}" je uspešno obrisana.')
